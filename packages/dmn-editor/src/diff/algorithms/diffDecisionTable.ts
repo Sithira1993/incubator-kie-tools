5aafae6778407d7b0d65e911c6c96c7f5eae05c0
Manuka Yasas <63389716+manuka99@users.noreply.github.com>
2025-12-30 12:54:43 +0530

    docs: remove unused diff viewer constants and algorithm utility.

--- packages/dmn-editor/src/diff/algorithms/diffDecisionTable.ts
+++ packages/dmn-editor/src/diff/algorithms/diffDecisionTable.ts
@@ -26,7 +26,7 @@ import {
 } from "@kie-tools/dmn-marshaller";
 import { BoxedDecisionTable } from "@kie-tools/boxed-expression-component/dist/api";
 import { BoxedExpressionDiff, DiffPropertyChange } from "../types";
-import { diffArrayElements, indexElementsById } from "./diffUtils";
+import { diffArrayElements } from "./diffUtils";
 
 /**
  * Represents a rule annotation entry in a Decision Table.
